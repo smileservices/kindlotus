97f09b487a56740969c81022b6588285a4b0c73c
vlad imi <vlad imi>
2016-09-07 12:57:33 +0300

    edit.js is ok

--- resources/views/cause/edit/js.blade.php
+++ resources/views/cause/edit/js.blade.php
@@ -5,8 +5,28 @@
 function setCoords(marker) {
     var lat = marker.latLng.lat();
     var lng = marker.latLng.lng();
+    var latLng = {
+        lat: lat,
+        lng: lng
+    };
     $('input#lat').val(lat);
     $('input#lng').val(lng);
+    return latLng;
+  }
+function setAddress(results) {
+    var address_components = results[1].address_components;
+    var country = results[results.length-1].formatted_address;
+    var area = results[results.length-2].address_components[0].short_name;
+    var city = results[results.length-3].address_components[0].short_name;
+    var size = address_components.length;
+    var address = {
+        country: country,
+        area: area,
+        city: city
+    };
+    $('input#country').val(address.country);
+    $('input#area').val(address.area);
+    $('input#city').val(address.city);
   }
 
 function initMap() {
@@ -28,8 +48,13 @@ function initMap() {
     draggable: true
   });
 
+  var geocoder = new google.maps.Geocoder;
+
   google.maps.event.addListener(marker, 'dragend', function(marker) {
-    setCoords(marker);
+    var latLng = setCoords(marker);
+    geocoder.geocode({'location': latLng}, function(results, status) {
+        setAddress(results);
+      });
   });
 
    $('#editDetailsButton').click(function(){
