4cc7120f0c2483867cb8fbdc5ae01226d8fff2ea
vlad imi <vlad imi>
2016-09-09 11:14:23 +0300

    footer ok

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,44 @@
 <footer>
-    <div class="container">
-    <div class="col-md-4">
-        © 2016 HelpMap
+    <div class="top">
+        <div class="container">
+            <div class="col-md-4">
+                <h4>Info</h4>
+                <ul class="list-unstyled">
+                    <li><a href="{{ url('about') }}">Despre</a></li>
+                    <li><a href="{{ url('about/us') }}">Pentru Organizatii</a></li>
+                    <li><a href="{{ url('about/guide') }}">Ghid de folosire</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h4>Contact</h4>
+                <ul class="list-unstyled">
+                    <li> <a href=""><i class="fa fa-envelope"> </i> Scrie-ne un mesaj</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 text-right">
+                <h4>Urmareste-ne</h4>
+                <a target="_blank" class="facebook_share" rel="nofollow" href="https://www.facebook.com/kindlotus">
+                    <i class="fa fa-2x fa-facebook-square primary-light-text-color" aria-hidden="true"> </i>
+                </a>
+                {{--<a target="_blank" href="">--}}
+                    {{--<i class="fa fa-2x fa-google-plus-square primary-light-text-color" aria-hidden="true"> </i>--}}
+                {{--</a>--}}
+                {{--<a target="_blank" href="">--}}
+                    {{--<i class="fa fa-2x fa-twitter-square primary-light-text-color" aria-hidden="true"> </i>--}}
+                {{--</a>--}}
+            </div>
+        </div>
     </div>
-    <div class="col-md-4">
-        Construit cu <i class="fa fa-heart"></i> de Smile Services
-    </div>
-    <div class="col-md-4">
-        {{--@include('layouts.socialShare', ['link' => url('/')])--}}
-    </div>
+    <div class="bottom">
+        <div class="container">
+            <div class="col-md-4">
+                © 2016 HelpMap
+            </div>
+            <div class="col-md-4">
+                Construit cu <i class="fa fa-heart"></i> de <a target="_blank" href="http://smile-services.ro">Smile Services</a>
+            </div>
+            <div class="col-md-4">
+            </div>
+        </div>
     </div>
 </footer>
